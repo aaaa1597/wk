a4ba22f0c75c80d09207b0fb811fe05ad85fbded
aaaaa <aaaaa>
2021-06-19 13:02:16 +0900

    クラス名変更。CG3DCom→com。長すぎ!!

--- AndCG3DViewer/app/src/main/cpp/CG3DCom.cpp
+++ AndCG3DViewer/app/src/main/cpp/CG3DCom.cpp
@@ -3,44 +3,46 @@
 //
 #include "CG3DCom.h"
 
-void CG3DCom::format_internal(std::stringstream &sout)
-{
-}
+namespace CG3D {
+    void com::format_internal(std::stringstream& sout)
+    {
+    }
 
-template<typename First, typename... Rest>
-void CG3DCom::format_internal(std::stringstream& sout, const First& first, const Rest&... rest)
-{
-    sout << first;
-    concat_internal(sout, rest...);
-}
+    template<typename First, typename... Rest>
+    void com::format_internal(std::stringstream& sout, const First& first, const Rest&... rest)
+    {
+        sout << first;
+        concat_internal(sout, rest...);
+    }
 
 
-template<typename... Args>
-std::string CG3DCom::format(const Args&... args)
-{
-    std::stringstream sout;
-    concat_internal(sout, args...);
-    return sout.str();
-}
+    template<typename... Args>
+    std::string com::format(const Args&... args)
+    {
+        std::stringstream sout;
+        concat_internal(sout, args...);
+        return sout.str();
+    }
 
 #ifndef __ANDROID__
-void CG3DCom::TRACE(LPCSTR pszFormat, ...)
-{
-    va_list	argp;
-    int len;
-    char* pszBuf;
+    void com::TRACE(LPCSTR pszFormat, ...)
+    {
+        va_list	argp;
+        int len;
+        char* pszBuf;
 
-    va_start(argp, pszFormat);
-    len = _vscprintf(pszFormat, argp) + 1;
-    if ((pszBuf = (char*)malloc(sizeof(char) * len)) != NULL) {
-        vsprintf_s(pszBuf, len, pszFormat, argp);
-        va_end(argp);
-        OutputDebugStringA(pszBuf);
-        free(pszBuf);
+        va_start(argp, pszFormat);
+        len = _vscprintf(pszFormat, argp) + 1;
+        if ((pszBuf = (char*)malloc(sizeof(char) * len)) != NULL) {
+            vsprintf_s(pszBuf, len, pszFormat, argp);
+            va_end(argp);
+            OutputDebugStringA(pszBuf);
+            free(pszBuf);
+        }
+        else {
+            va_end(argp);
+            OutputDebugStringA("ERROR: out of memory.\n");
+        }
     }
-    else {
-        va_end(argp);
-        OutputDebugStringA("ERROR: out of memory.\n");
-    }
-}
 #endif /* __ANDROID__*/
+}
--- AndCG3DViewer/app/src/main/cpp/CG3DCom.h
+++ AndCG3DViewer/app/src/main/cpp/CG3DCom.h
@@ -13,22 +13,24 @@
 
 #ifndef __ANDROID__
 #define ANDROID_LOG_DEBUG 0
-#define __android_log_print(m, n, fmt, ...)  {CG3DCom::TRACE(fmt, __VA_ARGS__) ; CG3DCom::TRACE("\n") ;}
+#define __android_log_print(m, n, fmt, ...)  {CG3D::com::TRACE(fmt, __VA_ARGS__) ; CG3D::com::TRACE("\n") ;}
 #endif /*__ANDROID__*/
 
-class CG3DCom {
-public:
-    template<typename... Args>
-    static std::string format(const Args&... args);
+namespace CG3D {
+    class com {
+    public:
+        template<typename... Args>
+        static std::string format(const Args&... args);
 #ifndef __ANDROID__
-    static void TRACE(LPCSTR pszFormat, ...);
+        static void TRACE(LPCSTR pszFormat, ...);
 #endif /*__ANDROID__*/
 
-private:
-    static void format_internal(std::stringstream &sout);
-    template<typename First, typename... Rest>
-    static void format_internal(std::stringstream &sout, const First &first, const Rest&... rest);
-};
+    private:
+        static void format_internal(std::stringstream& sout);
+        template<typename First, typename... Rest>
+        static void format_internal(std::stringstream& sout, const First& first, const Rest&... rest);
+    };
+}
 
 
 #endif //ANDCG3DVIEWER_CG3DCOM_H
